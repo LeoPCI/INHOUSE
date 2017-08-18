368b345cfca7b4a173990461a57f68464857cd08
Leo Bensman <work@Leos-MBP.workspace.codeandsupply.co>
2017-08-18 15:05:40 -0400

    added form validation and testing size changes

--- javascript.js
+++ javascript.js
@@ -20,14 +20,46 @@ $(window).keypress(function() {
   openPage();
 })
 
-// APPLICATION
 
-// $(function(){
-//   // $(".form > form").load("form-1.html"); 
-//   // $(".form > form").load("form-2.html"); 
-//   $(".form > form").load("form-1.html");
-//   $(".form > form").append("<button class='red-line'>SUBMIT</button>") 
-// });
+// HOVER SHOW ON MEMBERSHIP PAGE
+
+$('#hover-show-1').mouseenter(function() {
+  $('.hover-show-div').hide()
+  $('#hover-show-1-div').show()
+  $('.hover-show strong').removeClass('red')
+  $('#hover-show-1 strong').addClass('red')
+})
+
+
+$('#hover-show-2').mouseenter(function() {
+  $('.hover-show-div').hide()
+  $('#hover-show-2-div').show()
+  $('.hover-show strong').removeClass('red')
+  $('#hover-show-2 strong').addClass('red')
+})
+
+$('#hover-show-3').mouseenter(function() {
+  $('.hover-show-div').hide()
+  $('#hover-show-3-div').show()
+  $('.hover-show strong').removeClass('red')
+  $('#hover-show-3 strong').addClass('red')
+})
+
+$('#hover-show-4').mouseenter(function() {
+  $('.hover-show-div').hide()
+  $('#hover-show-4-div').show()
+  $('.hover-show strong').removeClass('red')
+  $('#hover-show-4 strong').addClass('red')
+})
+
+$('#hover-show-5').mouseenter(function() {
+  $('.hover-show-div').hide()
+  $('#hover-show-5-div').show()
+  $('.hover-show strong').removeClass('red')
+  $('#hover-show-5 strong').addClass('red')
+})
+
+// APPLICATION
 
 $('#invite-code').click(function(){
   $('#invite-select').css( "display", "block" )
@@ -35,40 +67,46 @@ $('#invite-code').click(function(){
 
 $('#get-form').click(function(){
 
-  if ($('#invite-select').val()=="F&F") {
+  $('#form-2').html('') //this is to prevent accidental form-2 staying loaded if offer certain code is applied
+
+  if ($('#invite-select').val().toUpperCase()=="F&F") {
     $('#form-1').load("form-1.html"); 
     $('#form-2').load("form-2.html");
     $('#form-3').load("form-3.html");
-    $('#price').html("<span class='striked'>$1,525</span><br>$750");
+    $('#price').html("<span class='striked'>$1,525 USD</span><br>$750 USD");
   }
 
-  else if ($('#invite-select').val()=="PULLUPACHAIR") {
+  else if ($('#invite-select').val().toUpperCase()=="PULLUPACHAIR") {
     $('#form-1').load("form-1.html"); 
     $('#form-2').load("form-2.html");
     $('#form-3').load("form-3.html");
-    $('#price').html("<span class='striked'>$1,525</span><br>$1,250");
+    $('#price').html("<span class='striked'>$1,525 USD</span><br>$1,250 USD");
   }
 
-  else if ($('#invite-select').val()=="WELCOMEHOME") {
+  else if ($('#invite-select').val().toUpperCase()=="WELCOMEHOME") {
     $('#form-1').load("form-1.html"); 
     $('#form-3').load("form-3.html");
-    $('#price').html("<span class='striked'>$1,525</span><br>$750");
+    $('#price').html("<span class='striked'>$1,525 USD</span><br>$750 USD");
   }
 
-  else if ($('#invite-select').val()=="FRIENDOFTHEHOUSE") {
+  else if ($('#invite-select').val().toUpperCase()=="FRIENDOFTHEHOUSE") {
     $('#form-1').load("form-1.html"); 
     $('#form-3').load("form-3.html");
-    $('#price').html("<span class='striked'>$1,525</span><br>$1,250");
+    $('#price').html("<span class='striked'>$1,525 USD</span><br>$1,250 USD");
   }
 
   else {
     $('#form-1').load("form-1.html"); 
     $('#form-2').load("form-2.html");
     $('#form-3').load("form-3.html");
-    $('#price').html("$1,525");
+    $('#price').html("$1,525 USD");
   }
 
   $('#submit-form-button').show();
+
+  $('html, body').animate({
+    scrollTop: $("form").offset().top
+  }, 1000);
 })
 
 // form onsubmit
@@ -147,66 +185,77 @@ function appendSheet(array) {
 }
 
 $('#SFB-button').click(function() {
-  done=false
 
-  // var values = [
-  //   $('#invite-select').val(), 
-  //   $('[name="firstName"]').val(),
-  // ];
+  if ($('[name="firstName"]').val()=='' || $('[name="lastName"]').val()=='' || $('[name="dateOfBirthMonth"]').val()=='' || $('[name="dateOfBirthDay"]').val()=='' || $('[name="dateOfBirthYear"]').val()=='' || $('[name="mailingAddressStreet"]').val()=='' || $('[name="mailingAddressCity"]').val()=='' || $('[name="mailingAddressState"]').val()=='' || $('[name="mailingAddressCountry"]').val()=='' || $('[name="phone"]').val()=='' || $('[name="email1"]').val()=='' || $('[name="email2"]').val()=='' || $('[name="companyEmployer"]').val()=='' || $('[name="companyTitle"]').val()=='' || $('[name="spouseFirstName"]').val()=='' || $('[name="spouseLastName"]').val()=='' || $('[name="spouseDateOfBirthMonth"]').val()=='' || $('[name="spouseDateOfBirthDay"]').val()=='' || $('[name="spouseDateOfBirthYear"]').val()=='' || $('[name="anniversaryMonth"]').val()=='' || $('[name="anniversaryDay"]').val()=='' || $('[name="anniversaryYear"]').val()=='') {
+    $('#alert-message-1').show();
+    $('html, body').animate({
+      scrollTop: $("#alert-message-1").offset().top
+    },  1000);
+  }
+  else if ($('[name="email1"]').val()!=$('[name="email2"]').val() ) {
+    $('#alert-message-2').show()
+    $('html, body').animate({
+      scrollTop: $("#alert-message-2").offset().top
+    },  1000);
+  }
+  else {
 
-  var values = [$('#invite-select').val(), 
-  $('[name="firstName"]').val(),
-  $('[name="lastName"]').val(),
-  $('[name="dateOfBirthMonth"]').val() + "/" + $('[name="dateOfBirthDay"]').val() + "/" + $('[name="dateOfBirthYear"]').val(),
-  $('[name="mailingAddressStreet"]').val() + ", " + $('[name="mailingAddressCity"]').val() + ", " + $('[name="mailingAddressState"]').val() + " " + $('[name="mailingAddressZip"]').val()  + ", " + $('[name="mailingAddressCountry"]').val(),
-  $('[name="phone"]').val(),
-  $('[name="email1"]').val(),
-  " ",
-  $('[name="companyEmployer"]').val(),
-  $('[name="companyTitle"]').val(),
-  " ",
-  $('[name="spouseFirstName"]').val(),
-  $('[name="spouseLastName"]').val(),
-  $('[name="spouseDateOfBirthMonth"]').val(),
-  $('[name="spouseDateOfBirthMonth"]').val() + "/" + $('[name="spouseDateOfBirthDay"]').val() + "/" + $('[name="spouseDateOfBirthYear"]').val(),
-  $('[name="anniversaryMonth"]').val() + "/" + $('[name="anniversaryDay"]').val() + "/" + $('[name="anniversaryYear"]').val(),
-  " ",
-  $('[name="noProposer"]').is(':checked'),
-  $('[name="restaurantProposerName"]').val(),
-  $('[name="restaurantProposerTitle"]').val(),
-  $('[name="restaurantProposerEmail"]').val(),
-  $('[name="currentMemberProposer"]').val(),
-  " ",
-  $('[name="dietaryRestrictions"]').val(),
-  $('[name="whereSit"]').val(),
-  $('[name="serviceStyle"]').val(),
-  " ",
-  $('[name="nycPicks1"]').val() +' '+ $('[name="nycPicks2"]').val() +', '+ $('[name="nycPicks3"]').val(),
-  $('[name="nycWhy1"]').val() +' '+ $('[name="nycWhy2"]').val() +', '+ $('[name="nycWhy3"]').val(),
-  " ",
-  $('[name="locals1"]').val() +' '+ $('[name="locals2"]').val() +', '+ $('[name="locals3"]').val(),
-  $('[name="localsWhy1"]').val() +' '+ $('[name="localsWhy2"]').val() +', '+ $('[name="localsWhy3"]').val(),
-  "hfdj"
-  ];
-  // var $inputs = $('form :input');
-  // $inputs.each(function() {
-  //   values.push($(this).val());
-  // });
+    done=false
 
-  console.log(values)
-  console.log(values[0])
-  appendSheet(values);
+    var values = [$('#invite-select').val(), 
+    $('[name="firstName"]').val(),
+    $('[name="lastName"]').val(),
+    $('[name="dateOfBirthMonth"]').val() + "/" + $('[name="dateOfBirthDay"]').val() + "/" + $('[name="dateOfBirthYear"]').val(),
+    $('[name="mailingAddressStreet"]').val() + ", " + $('[name="mailingAddressCity"]').val() + ", " + $('[name="mailingAddressState"]').val() + " " + $('[name="mailingAddressZip"]').val()  + ", " + $('[name="mailingAddressCountry"]').val(),
+    $('[name="phone"]').val(),
+    $('[name="email1"]').val(),
+    " ",
+    $('[name="companyEmployer"]').val(),
+    $('[name="companyTitle"]').val(),
+    " ",
+    $('[name="spouseFirstName"]').val(),
+    $('[name="spouseLastName"]').val(),
+    $('[name="spouseDateOfBirthMonth"]').val(),
+    $('[name="spouseDateOfBirthMonth"]').val() + "/" + $('[name="spouseDateOfBirthDay"]').val() + "/" + $('[name="spouseDateOfBirthYear"]').val(),
+    $('[name="anniversaryMonth"]').val() + "/" + $('[name="anniversaryDay"]').val() + "/" + $('[name="anniversaryYear"]').val(),
+    " ",
+    $('[name="noProposer"]').is(':checked'),
+    $('[name="restaurantProposerName"]').val(),
+    $('[name="restaurantProposerTitle"]').val(),
+    $('[name="restaurantProposerEmail"]').val(),
+    $('[name="currentMemberProposer"]').val(),
+    " ",
+    $('[name="dietaryRestrictions"]').val(),
+    // $('[name="whereSit"]').val(),
+    $('[name="serviceStyle"]').val(),
+    " ",
+    $('[name="nycPicks1"]').val() +' '+ $('[name="nycPicks2"]').val() +', '+ $('[name="nycPicks3"]').val(),
+    $('[name="nycWhy1"]').val() +' '+ $('[name="nycWhy2"]').val() +', '+ $('[name="nycWhy3"]').val(),
+    " ",
+    $('[name="locals1"]').val() +' '+ $('[name="locals2"]').val() +', '+ $('[name="locals3"]').val(),
+    $('[name="localsWhy1"]').val() +' '+ $('[name="localsWhy2"]').val() +', '+ $('[name="localsWhy3"]').val(),
+    "hfdj"
+    ];
+    // var $inputs = $('form :input');
+    // $inputs.each(function() {
+    //   values.push($(this).val());
+    // });
 
-  // setTimeout(doSomething, 100);
+    console.log(values)
+    console.log(values[0])
+    appendSheet(values);
 
-  // function doSomething() {
-  //   if (values[1]!="") {
-  //     console.log(values[0])
-  //     appendSheet(values);
-  //     // window.location = 'https://leopci.github.io/INHOUSE/membership.html'
-  //   };
-  // }
+    // setTimeout(doSomething, 100);
 
-  // appendSheet(["values",'aaa']);
-  return false
+    // function doSomething() {
+    //   if (values[1]!="") {
+    //     console.log(values[0])
+    //     appendSheet(values);
+    //     // window.location = 'https://leopci.github.io/INHOUSE/membership.html'
+    //   };
+    // }
+
+    // appendSheet(["values",'aaa']);
+    return false
+  }
 });
